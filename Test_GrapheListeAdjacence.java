98c0a8927e4997aa3893bee45870b700a716fda1
Noe0512 <noe.flipo@etu.u-paris.fr>
2026-04-07 17:31:45 +0200

    Add files via upload

---
+++ Test_GrapheListeAdjacence.java
@@ -0,0 +1,41 @@
+
+package Test;
+
+import Graphes.GrapheListe;
+
+public class Test_GrapheListeAdjacence {
+
+    public static void main(String[] args) {
+        GrapheListe monGraphe = new GrapheListe("Racine");
+
+        System.out.println("--- Test Ajout des Sommets ---");
+        monGraphe.addNode("A");
+        monGraphe.addNode("B");
+        monGraphe.addNode("C");
+        monGraphe.addNode("D");
+        monGraphe.nodes();
+
+        System.out.println("\n--- Test Ajout des Arêtes (A->B, B->C, C->D, A->D) ---");
+        monGraphe.addEdge("A", "B");
+        monGraphe.addEdge("B", "C");
+        monGraphe.addEdge("C", "D");
+        monGraphe.addEdge("A", "D");
+
+        System.out.println("\n--- Test de la méthode contains ---");
+        System.out.println("Contient A ? " + monGraphe.contains("A"));
+        System.out.println("Contient Z ? " + monGraphe.contains("Z"));
+
+        System.out.println("\n--- Test de la méthode neighbors (Voisins) ---");
+        // Doit afficher B et D
+        monGraphe.neighbors("A");
+        // Doit afficher C
+        monGraphe.neighbors("B");
+        // Doit afficher D
+        monGraphe.neighbors("C");
+        // Doit être vide
+        monGraphe.neighbors("D");
+
+        System.out.println("\n--- Test Erreur (Sommet inexistant) ---");
+        monGraphe.neighbors("Z");
+    }
+}
